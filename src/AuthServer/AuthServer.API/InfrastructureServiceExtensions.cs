c1ae124d42ca427490117668d156a5c5792ec1f9
Jonathan Thibault <Jothib@outlook.com>
2026-03-18 01:31:57 -0400

    added missing nuget

--- src/AuthServer/AuthServer.API/InfrastructureServiceExtensions.cs
+++ src/AuthServer/AuthServer.API/InfrastructureServiceExtensions.cs
@@ -36,7 +36,7 @@ public static class InfrastructureServiceExtensions
 			options.Password.RequiredLength = 8;
 			options.Password.RequireDigit = true;
 			options.Password.RequireUppercase = true;
-			options.Password.RequireNonAlphanumeric = false;
+			options.Password.RequireNonAlphanumeric = true;
 			options.Lockout.MaxFailedAccessAttempts = 5;
 			options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
 			options.User.RequireUniqueEmail = true;
